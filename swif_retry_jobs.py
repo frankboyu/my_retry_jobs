e68e61023369907397fa2022dfada38cad562c00
Bo Yu <by51@duke.edu>
2024-12-15 19:49:48 -0500

    Change the directory of swif2

--- swif_retry_jobs.py
+++ swif_retry_jobs.py
@@ -9,7 +9,7 @@ from collections import defaultdict, deque
 with open('/work/halld2/home/boyu/retry_jobs/retry_workflow.txt', 'r') as file:
     for line in file:
         workflow = line.strip()
-        command = "/usr/local/bin/swif2 retry-jobs -workflow " + workflow + " -problems SLURM_FAILED SLURM_CANCELLED SLURM_TIMEOUT SLURM_NODE_FAIL SLURM_OUT_OF_MEMORY SITE_LAUNCH_FAIL SITE_PREP_FAIL SWIF_INPUT_FAIL SWIF_SYSTEM_ERROR"
+        command = "/usr/bin/swif2 retry-jobs -workflow " + workflow + " -problems SLURM_FAILED SLURM_CANCELLED SLURM_TIMEOUT SLURM_NODE_FAIL SLURM_OUT_OF_MEMORY SITE_LAUNCH_FAIL SITE_PREP_FAIL SWIF_INPUT_FAIL SWIF_SYSTEM_ERROR"
         process = Popen(command.split(), stdout=PIPE)
         output = process.communicate()[0].decode('ASCII')
         print(workflow)
